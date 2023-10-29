4ffb6d2fe02a0678056882f25053300e18cbd9e2
Lucas Rodriguez <luucaasfb@hotmail.com>
2023-10-29 18:05:11 -0300

    PrimerPush

---
+++ Source/terrordefinjitivo/MyClassForSession.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class TERRORDEFINJITIVO_API MyClassForSession
+{
+public:
+	MyClassForSession();
+	~MyClassForSession();
+};
