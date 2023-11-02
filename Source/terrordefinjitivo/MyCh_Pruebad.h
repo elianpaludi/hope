860c3295df3ade12b2bd7131d52de99dee8cb25f
Lucas Rodriguez <luucaasfb@hotmail.com>
2023-11-02 15:19:40 -0300

    Inventario al 90%

---
+++ Source/terrordefinjitivo/MyCh_Pruebad.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ch_Prueba.h"
+#include "MyCh_Pruebad.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TERRORDEFINJITIVO_API AMyCh_Pruebad : public ACh_Prueba
+{
+	GENERATED_BODY()
+	
+};
